fa849c8e8e3a3495fa09aaec7b90d00b7cf2a8b0
Kipkoech99 <kipkoechevans99@gmail.com>
2024-04-24 22:11:46 +0300

    Deleted folder

--- 0x01-ES6_promise/1-promise.js
+++
@@ -1,9 +0,0 @@
-export default function getFullResponseFromAPI(success){
-	return new Promise((reject, resolve) => {
-		if (success) {
-			resolve({status: 200, body: 'Success'});
-		}; else {
-			reject new Error('The fake API is not working currently');
-		}
-	});
-}
